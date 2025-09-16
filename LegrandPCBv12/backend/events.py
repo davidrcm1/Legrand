7e9351258acdf135a654ac8b38ee2a27bc8e603f
davidrcm1 <73326239+davidrcm1@users.noreply.github.com>
2025-09-17 09:19:36 +1000

    V12
    Sequencer Actions x PCBS for either machine 1 or 5. all motion is relative and must be calibrated upon installation.
    
    #Recommendations
    - Limit NO. of machines to 1 per arm to avoid conflict.
    - Design a automated conveyor for individual pcb onloading. 
    - Connect to testing machine as a RX/TX Coms to identify passing and failed pcbs (dispose pcbs to individual trays.

---
+++ LegrandPCBv12/backend/events.py
@@ -0,0 +1,8 @@
+class BackendEvents:
+    STATUS = "STATUS"
+    PROGRAM_REQUEST = "program_request"
+    RUNNING = "running"
+    PAUSED = "paused"
+    STOPPED = "stopped"
+    FAULT = "fault"
+    LOG = "log"
