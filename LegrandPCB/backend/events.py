f1ec43e4ceb87865fb94225ceabcb09213882b61
davidrcm1 <73326239+davidrcm1@users.noreply.github.com>
2026-02-11 12:10:43 +1100

    Add files via upload
    Signed-off-by: davidrcm1 <73326239+davidrcm1@users.noreply.github.com>

---
+++ LegrandPCB/backend/events.py
@@ -0,0 +1,9 @@
+# exclusive ARM status Not testers
+class BackendEvents:
+    STATUS = "STATUS"
+    PROGRAM_REQUEST = "program_request"
+    RUNNING = "running"
+    PAUSED = "paused"
+    STOPPED = "stopped"
+    FAULT = "fault"
+    LOG = "log"
