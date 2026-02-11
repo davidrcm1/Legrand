deaa2586903ed0deea11727d96fbae4d59c8d721
davidrcm1 <73326239+davidrcm1@users.noreply.github.com>
2026-02-11 12:11:32 +1100

    Delete LegrandPCBv17 directory
    Signed-off-by: davidrcm1 <73326239+davidrcm1@users.noreply.github.com>

--- LegrandPCBv17/backend/events.py
+++
@@ -1,9 +0,0 @@
-# exclusive ARM status Not testers
-class BackendEvents:
-    STATUS = "STATUS"
-    PROGRAM_REQUEST = "program_request"
-    RUNNING = "running"
-    PAUSED = "paused"
-    STOPPED = "stopped"
-    FAULT = "fault"
-    LOG = "log"
